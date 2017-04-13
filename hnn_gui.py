0a8a779b192edf32736f0bdd5b89764e90913f45
Sam Neymotin <samnemo@gmail.com>
2017-04-13 12:40:50 -0400

    update neuron-ui interface

--- hnn_gui.py
+++ hnn_gui.py
@@ -1,9 +1,9 @@
 import logging
 from neuron import h
 
-from geppettoJupyter.geppetto_comm import GeppettoCoreAPI as G
-import neuron_utils
-import neuron_geometries_utils
+from jupyter_geppetto.geppetto_comm import GeppettoCoreAPI as G
+from neuron_ui import neuron_utils
+from neuron_ui import neuron_geometries_utils
 
 class HNN:
 
@@ -15,7 +15,7 @@ class HNN:
         self.t_vec = h.Vector()
         self.t_vec.record(h._ref_t)
         neuron_utils.createStateVariable(id='time', name='time',
-                              units='ms', python_variable={"record_variable": self.t_vec,
+                          units='ms', python_variable={"record_variable": self.t_vec,
                                                            "segment": None})
 
         neuron_geometries_utils.extractGeometries()
