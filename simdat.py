86ab7d4600896f178ff8267a4d36567c8a595b1d
Sam Neymotin <samnemo@gmail.com>
2017-04-17 12:58:43 -0400

    move sim data drawing into separate function

--- simdat.py
+++ simdat.py
@@ -38,9 +38,66 @@ if debug:
   except:
     print('exception in getting input files')
 
+invertedhistax = False
+
+def plotsimdat (figure,G,fig):
+  global invertedhistax
+  if len(ddat.keys()) == 0: return
+  try:
+    fig,ax = plt.subplots(); ax.cla()
+    xlim_new = (ddat['dpl'][0,0],ddat['dpl'][-1,0])
+
+    # set number of bins (150 bins per 1000ms)
+    bins = ceil(150. * (xlim_new[1] - xlim_new[0]) / 1000.) # bins needs to be an int
+
+    # plot histograms of inputs
+    print(dfile['spk'],dfile['outparam'])
+    extinputs = None
+
+    try:
+      extinputs = spikefn.ExtInputs(dfile['spk'], dfile['outparam'])
+      extinputs.add_delay_times()
+    except:
+      print('problem with extinputs')
+
+    hist = {}
+    axdist = figure.add_subplot(G[0,0]); axdist.cla() # distal inputs
+    axprox = figure.add_subplot(G[1,0]); axprox.cla() # proximal inputs
+    if extinputs is not None: # only valid param.txt file after sim was run
+      hist['feed_dist'] = extinputs.plot_hist(axdist,'dist',ddat['dpl'][:,0],bins,xlim_new,color='r')
+      hist['feed_prox'] = extinputs.plot_hist(axprox,'prox',ddat['dpl'][:,0],bins,xlim_new,color='g')
+      if not invertedhistax:# only need to invert axis 1X
+        axdist.invert_yaxis()
+        invertedhistax = True
+      for ax in [axdist,axprox]:
+        ax.set_xlim(xlim_new)
+        ax.legend()          
+
+    ax = figure.add_subplot(G[2:5,0]); ax.cla() # dipole
+    ax.plot(ddat['dpl'][:,0],ddat['dpl'][:,1],'b')
+    ax.set_ylabel('dipole (nA m)')
+    ax.set_xlim(ddat['dpl'][0,0],ddat['dpl'][-1,0])
+    ax.set_ylim(np.amin(ddat['dpl'][:,1]),np.amax(ddat['dpl'][:,1])) # right ylim??
+    print('ylim is : ', np.amin(ddat['dpl'][:,1]),np.amax(ddat['dpl'][:,1]))
+    # truncate tvec and dpl data using logical indexing
+    #t_range = dpl.t[(dpl.t >= xmin) & (dpl.t <= xmax)]
+    #dpl_range = dpl.dpl['agg'][(dpl.t >= xmin) & (dpl.t <= xmax)]
+
+    ax = figure.add_subplot(G[6:10,0]); ax.cla() # specgram
+    ds = ddat['spec']
+    cax = ax.imshow(ds['TFR'],extent=(ds['time'][0],ds['time'][-1],ds['freq'][-1],ds['freq'][0]),aspect='auto',origin='upper',cmap=plt.get_cmap('jet'))
+    ax.set_ylabel('Frequency (Hz)')
+    ax.set_xlabel('Time (ms)')
+    ax.set_xlim(ds['time'][0],ds['time'][-1])
+    ax.set_ylim(ds['freq'][-1],ds['freq'][0])
+    cbaxes = fig.add_axes([0.915, 0.125, 0.03, 0.2]) 
+    cb = plt.colorbar(cax, cax = cbaxes)  
+    #self.fig.tight_layout() # tight_layout will mess up colorbar location
+  except:
+    print('ERR: in plot')
+
 # based on https://pythonspot.com/en/pyqt5-matplotlib/
 class SIMCanvas (FigureCanvas): 
-
   def __init__ (self, parent=None, width=5, height=4, dpi=100, title='Simulation Viewer'):
     self.fig = fig = Figure(figsize=(width, height), dpi=dpi)
     self.title = title
@@ -49,61 +106,7 @@ class SIMCanvas (FigureCanvas):
     FigureCanvas.setSizePolicy(self,QSizePolicy.Expanding,QSizePolicy.Expanding)
     FigureCanvas.updateGeometry(self)
     self.G = gridspec.GridSpec(10,1)
-    self.invertedhistax = False
     self.plot()
-
   def plot (self):
-    if len(ddat.keys()) == 0: return
-    try:
-      fig,ax = plt.subplots(); ax.cla()
-      xlim_new = (ddat['dpl'][0,0],ddat['dpl'][-1,0])
-
-      # set number of bins (150 bins per 1000ms)
-      bins = ceil(150. * (xlim_new[1] - xlim_new[0]) / 1000.) # bins needs to be an int
-
-      # plot histograms of inputs
-      print(dfile['spk'],dfile['outparam'])
-      extinputs = None
-
-      try:
-        extinputs = spikefn.ExtInputs(dfile['spk'], dfile['outparam'])
-        extinputs.add_delay_times()
-      except:
-        print('problem with extinputs')
-
-      hist = {}
-      axdist = self.figure.add_subplot(self.G[0,0]); axdist.cla() # distal inputs
-      axprox = self.figure.add_subplot(self.G[1,0]); axprox.cla() # proximal inputs
-      if extinputs is not None: # only valid param.txt file after sim was run
-        hist['feed_dist'] = extinputs.plot_hist(axdist,'dist',ddat['dpl'][:,0],bins,xlim_new,color='r')
-        hist['feed_prox'] = extinputs.plot_hist(axprox,'prox',ddat['dpl'][:,0],bins,xlim_new,color='g')
-        if not self.invertedhistax:# only need to invert axis 1X
-          axdist.invert_yaxis()
-          self.invertedhistax = True
-        for ax in [axdist,axprox]:
-          ax.set_xlim(xlim_new)
-          ax.legend()          
-
-      ax = self.figure.add_subplot(self.G[2:5,0]); ax.cla() # dipole
-      ax.plot(ddat['dpl'][:,0],ddat['dpl'][:,1],'b')
-      ax.set_ylabel('dipole (nA m)')
-      ax.set_xlim(ddat['dpl'][0,0],ddat['dpl'][-1,0])
-      ax.set_ylim(np.amin(ddat['dpl'][:,1]),np.amax(ddat['dpl'][:,1])) # right ylim??
-      print('ylim is : ', np.amin(ddat['dpl'][:,1]),np.amax(ddat['dpl'][:,1]))
-      # truncate tvec and dpl data using logical indexing
-      #t_range = dpl.t[(dpl.t >= xmin) & (dpl.t <= xmax)]
-      #dpl_range = dpl.dpl['agg'][(dpl.t >= xmin) & (dpl.t <= xmax)]
-
-      ax = self.figure.add_subplot(self.G[6:10,0]); ax.cla() # specgram
-      ds = ddat['spec']
-      cax = ax.imshow(ds['TFR'],extent=(ds['time'][0],ds['time'][-1],ds['freq'][-1],ds['freq'][0]),aspect='auto',origin='upper',cmap=plt.get_cmap('jet'))
-      ax.set_ylabel('Frequency (Hz)')
-      ax.set_xlabel('Time (ms)')
-      ax.set_xlim(ds['time'][0],ds['time'][-1])
-      ax.set_ylim(ds['freq'][-1],ds['freq'][0])
-      cbaxes = self.fig.add_axes([0.915, 0.125, 0.03, 0.2]) 
-      cb = plt.colorbar(cax, cax = cbaxes)  
-      #self.fig.tight_layout() # tight_layout will mess up colorbar location
-    except:
-      print('ERR: in plot')
+    plotsimdat(self.figure,self.G,self.fig)
     self.draw()
