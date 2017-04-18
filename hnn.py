a47e8476b9bcd046456bc12ecc3cd469c10645d9
Sam Neymotin <samnemo@gmail.com>
2017-04-18 16:15:16 -0400

    get model to load into NEURON-UI through jupyter run

--- hnn.py
+++ hnn.py
@@ -9,27 +9,16 @@ from time import sleep
 
 from hnn_qt5 import *
 
-from signal import signal, SIGPIPE, SIG_DFL
-
 #
 def runnrnui ():
-  signal(SIGPIPE,SIG_DFL) 
   pnrnui = Popen(shlex.split(os.path.join('NEURON-UI','NEURON-UI')),cwd=os.getcwd())
-  sleep(7)
-  pjup = Popen(shlex.split('jupyter console --existing'),cwd=os.getcwd(),stdin=PIPE,stdout=PIPE,stderr=PIPE,shell=True)
-  sleep(5)
-  lproc = [pnrnui,pjup]
-  lns = ["import os\n",
-         "os.chdir('NEURON-UI/neuron_ui/models/hnn')\n",
-         "import hnn_nrnui\n",
-         "net=hnn_nrnui.HNN()\n"]
-  for s in lns:
-    pjup.stdin.write(s.encode())
-    sleep(1)
-    pjup.stdin.flush()
-    sleep(1)
-    #print(pjup.communicate(input=s.encode())[0])
-    while pjup.poll() is None: pass
+  sleep(5) # make sure NEURON-UI had a chance to start
+  pjup = Popen(shlex.split('jupyter run loadmodel_nrnui.py --existing'),cwd=os.getcwd())
+  lproc = [pnrnui, pjup]; done = [False, False]
+  while pnrnui.poll() is None or pjup.poll() is None: sleep(1)
+  for p in lproc:
+    try: p.communicate()
+    except: pass
   return lproc
 
 def runqt5 ():
