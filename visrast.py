6fb420d066730cb84a8204b917a9beb042e355e5
Sam Neymotin <samnemo@gmail.com>
2017-05-03 12:32:07 -0400

    separate inputs from cell spiking

--- visrast.py
+++ visrast.py
@@ -25,28 +25,47 @@ for i in range(len(sys.argv)):
     paramf = sys.argv[i]
 
 ddat = {}
-ddat['spk'] = np.loadtxt(spkpath)
-t,gid,clr=[],[],[]
-for pair in ddat['spk']:
-  t.append(pair[0])
-  gid.append(pair[1])  
-  ty = net.gid_to_type(gid[-1])
-  if ty in dclr:
-    clr.append(dclr[ty])
-  else:
-    clr.append('w')
-
-def handle_close (evt):
+try:
+  ddat['spk'] = np.loadtxt(spkpath)
+except:
+  print('Could not load',spkpath)
   quit()
+
+dspk = {'Cell':([],[],[]),'Input':([],[],[])}
+
+lt,lgid,lclr=[],[],[]
+ncell = len(net.cells)
+haveinputs = False
+for (t,gid) in ddat['spk']:
+  ty = net.gid_to_type(gid)
+  if ty in dclr:
+    dspk['Cell'][0].append(t)
+    dspk['Cell'][1].append(gid)
+    dspk['Cell'][2].append(dclr[ty])
+  else:
+    dspk['Input'][0].append(t)
+    dspk['Input'][1].append(gid)
+    dspk['Input'][2].append('w')
+    haveinputs = True
+
+def handle_close (evt): quit()
 
 if __name__ == '__main__':
   plt.ion()
   fig = plt.figure()
   fig.canvas.mpl_connect('close_event', handle_close)
-  ax=plt.gca()
-  ax.scatter(t,gid,c=clr,s=8**2)
-  plt.xlabel('Time (ms)'); plt.ylabel('ID')
-  ax.set_facecolor('k')
-  ax.grid(True)
-  ax.set_title('Raster Plot')
+  lk = ['Cell']
+  gdx = 111
+  if haveinputs:
+    lk.append('Input')
+    gdx = 211
+  for i,k in enumerate(lk):
+    ax=fig.add_subplot(gdx)
+    ax.scatter(dspk[k][0],dspk[k][1],c=dspk[k][2],s=8**2)
+    plt.xlabel('Time (ms)');
+    plt.ylabel(k + ' ID')
+    ax.set_facecolor('k')
+    ax.grid(True)
+    if i ==0: ax.set_title('Raster Plot')
+    gdx += 1
 
