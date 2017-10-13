e8d281303ae0d888cca5135da2df900a2fe795c3
salvadord <salvadordura@gmail.com>
2017-10-13 18:38:03 -0400

    modified L2_pyr cell

--- init.py
+++ init.py
@@ -1,9 +1,9 @@
-# init.py - Starting script to run NetPyNE-based model.
+in# init.py - Starting script to run NetPyNE-based model.
 # Usage:  python init.py  # Run simulation, optionally plot a raster
 # MPI usage:  mpiexec -n 4 nrniv -python -mpi init.py
 
 from netpyne import sim
 cfg, netParams = sim.readCmdLineArgs('cfg.py','netParams.py') # read cfg and netParams from command line arguments
-sim.create(simConfig=cfg, netParams=netParams)
-#sim.createSimulateAnalyze(simConfig = cfg, netParams = netParams) 
+#sim.create(simConfig=cfg, netParams=netParams)
+sim.createSimulateAnalyze(simConfig = cfg, netParams = netParams) 
 
