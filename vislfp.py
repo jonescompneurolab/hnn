253d1f98cff08680f9dbad66e7a7607d7500b0e9
Sam Neymotin <samnemo@gmail.com>
2017-10-26 16:12:16 -0400

    working on lfp drawing

--- vislfp.py
+++ vislfp.py
@@ -23,8 +23,9 @@ import spikefn
 from math import ceil
 from simdat import readdpltrials
 from conf import dconf
+from specfn import MorletSpec
 
-tstop = -1; ntrial = 0; scalefctr = 30e3; lfppath = ''; paramf = ''
+tstop = -1; ntrial = 0; maxlfp = 0; scalefctr = 30e3; lfppath = ''; paramf = ''
 for i in range(len(sys.argv)):
   if sys.argv[i].endswith('.txt'):
     lfppath = sys.argv[i]
@@ -39,11 +40,59 @@ basedir = os.path.join(dconf['datdir'],paramf.split(os.path.sep)[-1].split('.par
 
 ddat = {}
 ddat['dpltrials'] = readdpltrials(basedir,ntrial)
+
+def readLFPs (basedir, ntrial):
+  ddat = {'lfp':{}}
+  lfile = os.listdir(basedir)
+  maxlfp = 0
+  print(lfile)
+  for f in lfile:
+    if f.count('lfp_') > 0 and f.endswith('.txt'):
+      lf = f.split('.txt')[0].split('_')
+      print(lf,ntrial)
+      if ntrial > 0:
+        trial = int(lf[1])
+        nlfp = int(lf[2])
+      else:
+        trial = 0
+        nlfp = int(lf[1])
+      maxlfp = max(nlfp,maxlfp)
+      print(trial,nlfp,maxlfp)
+      fullpath = os.path.join(basedir,f)
+      print(fullpath)
+      try:
+        k2 = (trial,nlfp)
+        print('k2:',k2)
+        ddat['lfp'][k2] = np.loadtxt(fullpath)
+      except:
+        print('exception!')
+      print(ddat['lfp'].keys())
+  print('ddat:',ddat,maxlfp)
+  return ddat, maxlfp
+
 try:
-  ddat['lfp'] = np.loadtxt(os.path.join(basedir,'lfp.txt'))
+  ddat, maxlfp = readLFPs(basedir,ntrial) # np.loadtxt(os.path.join(basedir,'lfp.txt'))
 except:
-  print('Could not load',lfppath)
+  print('Could not load LFPs')#,lfppath)
   quit()
+
+# assumes column 0 is time, rest of columns are time-series
+def extractspec (dat, fmax=120.0):
+  global ntrial
+  print('extractspec',dat.shape)
+  lspec = []
+  tvec = dat[:,0]
+  dt = tvec[1] - tvec[0]
+  tstop = tvec[-1]
+  print('tstop is ', tstop)
+  prm = {'f_max_spec':fmax,'dt':dt,'tstop':tstop}
+  for col in range(1,dat.shape[1],1):
+    ms = MorletSpec(tvec,dat[:,col],None,None,prm)
+    lspec.append(ms)
+  ntrial = len(lspec)
+  #avgdipole = np.mean(dat[:,1:-1],axis=1)
+  #avgspec = MorletSpec(tvec,avgdipole,None,None,prm)
+  return ms.f, lspec, avgdipole, avgspec
 
 class LFPCanvas (FigureCanvas):
 
@@ -67,32 +116,48 @@ class LFPCanvas (FigureCanvas):
 
   def drawLFP (self, fig):
 
-    gdx = 311
+    nrow = (maxlfp+1) * 2
+    ncol = 1
+    gdx = 1
 
-    ltitle = ['Layer2', 'Layer5', 'Aggregate']
+    # ltitle = ['Layer2', 'Layer5', 'Aggregate']
+    ltitle = ['LFP'+str(x) for x in range(nrow)]
 
     white_patch = mpatches.Patch(color='white', label='Average')
     gray_patch = mpatches.Patch(color='gray', label='Individual')
     lpatch = []
 
-    if len(ddat['dpltrials']) > 0: lpatch = [white_patch,gray_patch]
+    if ntrial > 0: lpatch = [white_patch,gray_patch]
 
     yl = [1e9,-1e9]
     
     for i in [1]:
-      yl[0] = min(yl[0],ddat['lfp'][:,i].min())
-      yl[1] = max(yl[1],ddat['lfp'][:,i].max())
+      print('ddat[lfp].keys():',ddat['lfp'].keys())
+      for k in ddat['lfp'].keys():
+        print('drawkey',k)
+        yl[0] = min(yl[0],ddat['lfp'][k][:,i].min())
+        yl[1] = max(yl[1],ddat['lfp'][k][:,i].max())
+      """
       if len(ddat['dpltrials']) > 0: # plot LFP from individual trials
         for dpltrial in ddat['dpltrials']:
           yl[0] = min(yl[0],dpltrial[:,i].min())
           yl[1] = max(yl[1],dpltrial[:,i].max())
+      """
 
     yl = tuple(yl)
 
     self.lax = []
 
-    for i,title in zip([1],ltitle):
-      ax = fig.add_subplot(gdx)
+    for k in ddat['lfp'].keys():
+      trial,nlfp = k
+      gdx = nlfp * 2 + 1
+      title = ltitle[nlfp]
+
+      i = 1
+
+      print('row,col,gdx',nrow,ncol,gdx)
+
+      ax = fig.add_subplot(nrow,ncol,gdx)
       self.lax.append(ax)
 
       if i == 1: ax.set_xlabel('Time (ms)');
@@ -100,24 +165,28 @@ class LFPCanvas (FigureCanvas):
       lw = 2
       if self.index != 0: lw = 5
 
+      """
       if len(ddat['dpltrials']) > 0: # plot LFP from individual trials
         for ddx,dpltrial in enumerate(ddat['dpltrials']):
           if self.index == 0 or ddx == self.index-1:
             ax.plot(dpltrial[:,0],dpltrial[:,i],color='gray',linewidth=lw)
+      """
 
-      if self.index == 0: ax.plot(ddat['lfp'][:,0],ddat['lfp'][:,i],'w',linewidth=5)
+      if self.index == 0: ax.plot(ddat['lfp'][k][:,0],ddat['lfp'][k][:,i],'w',linewidth=5)
 
       # ax.set_ylabel(r'(nAm $\times$ '+str(scalefctr)+')')
       if tstop != -1: ax.set_xlim((0,tstop))
       ax.set_ylim(yl)
 
-      if i == 2 and len(ddat['dpltrials']) > 0: plt.legend(handles=lpatch)
+      # if i == 2 and len(ddat['dpltrials']) > 0: plt.legend(handles=lpatch)
 
       ax.set_facecolor('k')
       ax.grid(True)
       ax.set_title(title)
 
       gdx += 1
+
+      # plot wavelet transform here
 
   def plot (self):
     self.drawLFP(self.figure)
