1aac9846e7711558fdf3cefd79f7473edb648890
salvadord <salvadordura@gmail.com>
2018-12-14 20:29:40 -0500

    working on L5Pyr params

--- cellParams.py
+++ cellParams.py
@@ -166,68 +166,68 @@ cellParams['L5Pyr_rule'] = {
             'basal': ['basal_1', 'basal_2', 'basal_3']},
         'secs': {
             'soma': {
-                'geom': {'L': 39.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 28.899999618530273, 'nseg': 1,
-                    'pt3d': [[0.0, 0.0, 0.0, 28.899999618530273],
-                        [0.0, 39.0, 0.0, 28.899999618530273]]},
+                'geom': {'L': cfg.L5Pyr_soma_L, 'Ra': cfg.L5Pyr_soma_Ra, 'cm': cfg.L5Pyr_soma_cm, 'diam': cfg.L5Pyr_soma_diam, 'nseg': 1,
+                    'pt3d': [[0.0, 0.0, 0.0, cfg.L5Pyr_soma_diam],
+                        [0.0, 0.0+cfg.L5Pyr_soma_diam, 0.0, cfg.L5Pyr_soma_diam]]},
                 'ions': {
                     'ca': {'e': 132.4579341637009, 'i': 5e-05, 'o': 2.0},
                     'k': {'e': -77.0, 'i': 54.4, 'o': 2.5},
                     'na': {'e': 50.0, 'i': 10.0, 'o': 140.0}},
                 'mechs': {
-                    'ar': {'gbar': 1e-06}, 
-                    'ca': {'gbar': 60.0},
-                    'cad': {'taur': 20.0},
-                    'cat': {'gbar': 0.0002},
+                    'ar': {'gbar': cfg.L5Pyr_soma_gbar_ar}, 
+                    'ca': {'gbar': cfg.L5Pyr_soma_gbar_ca},
+                    'cad': {'taur': cfg.L5Pyr_soma_gbar_cad},
+                    'cat': {'gbar': cfg.L5Pyr_soma_gbar_cat},
                     'dipole': {},
-                    'hh2': {'el': -65.0, 'gkbar': 0.01, 'gl': 4.26e-05, 'gnabar': 0.16},
-                    'kca': {'gbar': 0.0002},
-                    'km': {'gbar': 200.0}},
+                    'hh2': {'el': cfg.L5Pyr_soma_el_hh2, 'gkbar': cfg.L5Pyr_soma_gkbar_hh2, 'gl': cfg.L5Pyr_soma_gl_hh2, 'gnabar': cfg.L5Pyr_soma_gnabar_hh2},
+                    'kca': {'gbar': cfg.L5Pyr_soma_gbar_kc2},
+                    'km': {'gbar': cfg.L5Pyr_soma_gbar_km}},
                 'topol': {}
             },
             'apical_1': {
-                'geom': {'L': 680.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 7.48000001907348, 'nseg': 13,
+                'geom': {'L': 680.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 13,
                     'pt3d': [[0.0, 83.0, 0.0, 7.480000019073486],
                         [0.0, 763.0, 0.0, 7.480000019073486]]},
                 'topol': {'childX': 0.0, 'parentSec': 'apical_trunk','parentX': 1.0}
             },
             'apical_2': {
-                'geom': {'L': 680.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 4.929999828338619, 'nseg': 13,
+                'geom': {'L': 680.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 13,
                     'pt3d': [[0.0, 483.0, 0.0, 4.929999828338623],
                         [0.0, 1163.0, 0.0, 4.929999828338623]]},
                 'topol': {'childX': 0.0, 'parentSec': 'apical_1', 'parentX': 1.0}
             },
             'apical_oblique': {
-                'geom': {'L': 255.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 5.099999904632568, 'nseg': 5,
+                'geom': {'L': 255.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 5,
                     'pt3d': [[0.0, 83.0, 0.0, 5.099999904632568],
                         [-255.0, 83.0, 0.0, 5.099999904632568]]},
                 'topol': {'childX': 0.0, 'parentSec': 'apical_trunk', 'parentX': 1.0}
             },
             'apical_trunk': {
-                'geom': {'L': 102.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 10.199999809265137, 'nseg': 3,
+                'geom': {'L': 102.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 3,
                     'pt3d': [[0.0, 23.0, 0.0, 10.199999809265137 ],
                         [0.0, 125.0, 0.0, 10.199999809265137]]},
                 'topol': {'childX': 0.0, 'parentSec': 'soma', 'parentX': 1.0}
             },
             'apical_tuft': {
-                'geom': {'L': 425.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 3.400000095367432, 'nseg': 9,
+                'geom': {'L': 425.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 9,
                     'pt3d': [[0.0, 883.0, 0.0, 3.4000000953674316],
                         [0.0, 1308.0, 0.0, 3.4000000953674316]]},
                 'topol': {'childX': 0.0, 'parentSec': 'apical_2', 'parentX': 1.0}
             },
             'basal_1': {
-                'geom': {'L': 85.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 6.800000190734863, 'nseg': 1,
+                'geom': {'L': 85.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 1,
                     'pt3d': [[0.0, 0.0, 0.0, 6.800000190734863],
                         [0.0, -85.0, 0.0, 6.800000190734863]]},
                 'topol': {'childX': 0.0, 'parentSec': 'soma', 'parentX': 0.0}
             },
             'basal_2': {
-                'geom': {'L': 255.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 8.5, 'nseg': 5,
+                'geom': {'L': 255.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 5,
                     'pt3d': [[0.0, -50.0, 0.0, 8.5],
                         [-180.31222534179688, -230.31222534179688, 0.0, 8.5]]},
                 'topol': {'childX': 0.0, 'parentSec': 'basal_1', 'parentX': 1.0}
             },
             'basal_3': {
-                'geom': {'L': 255.0, 'Ra': 200.0, 'cm': 0.85, 'diam': 8.5, 'nseg': 5,
+                'geom': {'L': 255.0, 'Ra': cfg.L5Pyr_dend_Ra, 'cm': cfg.L5Pyr_dend_cm, 'diam': cfg.L5Pyr_dend_diam, 'nseg': 5,
                     'pt3d': [[0.0, -50.0, 0.0, 8.5],
                         [180.31222534179688, -230.31222534179688, 0.0, 8.5]]},
                 'topol': {
