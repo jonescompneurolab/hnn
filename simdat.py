BUG: fix y-scaling when data values are very small

simdat.py
@@ -406,8 +406,9 @@ class SIMCanvas (FigureCanvas):
 
     if self.axdipole is None:
       self.axdipole = self.figure.add_subplot(self.G[0:-1,0]) # dipole
-
-    yl = self.axdipole.get_ylim()
+      yl = (-0.001,0.001)
+    else:
+      yl = self.axdipole.get_ylim()
 
     cmap=plt.get_cmap('nipy_spectral')
     csm = plt.cm.ScalarMappable(cmap=cmap);
@@ -453,7 +454,7 @@ class SIMCanvas (FigureCanvas):
     if not hassimdata: # need axis labels
       left = 0.08
       w,h=getscreengeom()
-      if w < 2800: left = 0.1
+      if w < 2800: left = 0.12
       self.axdipole.set_xlabel('Time (ms)',fontsize=dconf['fontsize'])
       self.axdipole.set_ylabel('Dipole (nAm)',fontsize=dconf['fontsize'])
       myxl = self.axdipole.get_xlim()
@@ -530,7 +531,7 @@ class SIMCanvas (FigureCanvas):
     N_trials = self.getNTrials()
     if debug: print('simdat: N_trials:',N_trials)
 
-    yl = list(self.axdipole.get_ylim())
+    yl = [0,0]
     yl[0] = min(yl[0],np.amin(ddat['dpl'][sidx:eidx,1]))
     yl[1] = max(yl[1],np.amax(ddat['dpl'][sidx:eidx,1]))
 
@@ -582,7 +583,7 @@ class SIMCanvas (FigureCanvas):
     bottom = 0.0
     left = 0.08
     w,h=getscreengeom()
-    if w < 2800: left = 0.1
+    if w < 2800: left = 0.13
 
     if DrawSpec: #
       if debug: print('ylim is : ', np.amin(ddat['dpl'][sidx:eidx,1]),np.amax(ddat['dpl'][sidx:eidx,1]))