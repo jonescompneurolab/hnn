c318769f32b5af81836b472393be7b4078869d0e
Blake Caldwell <blake_caldwell@brown.edu>
2019-08-16 20:00:41 -0400

    Fix start_hhn.sh for Oscar

--- installer/docker/start_hnn.sh
+++ installer/docker/start_hnn.sh
@@ -3,17 +3,26 @@
 if [ ! -d /home/hnn_user/hnn ]; then
   echo "creating /home/hnn_user/hnn"
   mkdir /home/hnn_user/hnn
-else
-  echo "changing ownership permissions of /home/hnn_user/hnn"
-  sudo chown -R hnn_user:hnn_group /home/hnn_user/hnn
 fi
 
 cd /home/hnn_user/hnn_repo
 
+if [[ ! "$(ulimit -l)" =~ "unlimited" ]]; then
+  ulimit -l unlimited
+  if [[ "$?" -eq "0" ]]; then
+    echo "** Successfully increased max locked memory (for HNN) **"
+  else
+    echo "** Failed to increase max locked memory (for HNN) **"
+  fi
+fi
+
+# try once with default DISPLAY
+python3 hnn.py hnn.cfg
+
 done=
 XHOST=${DISPLAY%:0}
 # try some common hosts
-for XHOST in $XHOST 192.168.99.1 192.168.65.2 ""; do
+for XHOST in "" $XHOST 192.168.99.1 192.168.65.2 ""; do
   for PORT in 0 1 2 3 4; do
     export DISPLAY=$XHOST:$PORT
     echo "Trying to start HNN with DISPLAY=$DISPLAY"
