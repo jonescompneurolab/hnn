bd2019a067dcee31992742c51d3337ef197597a8
Blake Caldwell <blake_caldwell@brown.edu>
2020-03-28 12:28:38 -0400

    MAINT: prepare for hnn_docker.sh update
    Instead of changing permissions of ssh files outside of the container
    with hnn_docker.sh, do it on every container start (when sshd is
    started)

--- installer/docker/start_ssh.sh
+++ installer/docker/start_ssh.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# set permissions for these files in case they were copied in by the
+# start script since last start
+if [ -e "/home/hnn_user/.ssh/authorized_keys" ]; then
+    chown hnn_user /home/hnn_user/.ssh/authorized_keys
+    chmod 600 /home/hnn_user/.ssh/authorized_keys
+fi
+if [ -e "/home/hnn_user/.ssh/known_hosts" ]; then
+    chown hnn_user:hnn_group /home/hnn_user/.ssh/known_hosts
+fi
+
 echo "HNN container has started. To start the HNN GUI run the following command:"
 echo "docker exec hnn_container /home/hnn_user/start_hnn.sh"
 
