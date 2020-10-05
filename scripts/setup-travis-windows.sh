a6e856cca4d31e8703f8ed1e8e6e5863c0a977fa
Blake Caldwell <blake_caldwell@brown.edu>
2020-10-05 18:20:40 -0400

    TST: fix WSL testing by adding VcXsrv to PATH
    Move around code that creates a new user with a space "test user"
    to setup-travis-windows.sh run running py.test as a different user.

--- scripts/setup-travis-windows.sh
+++ scripts/setup-travis-windows.sh
@@ -9,4 +9,31 @@ powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://git
 echo "Running HNN Windows install script..."
 powershell.exe -ExecutionPolicy Bypass -File ./installer/windows/hnn-windows.ps1
 
-source "$HOME/Miniconda3/etc/profile.d/conda.sh"
+# enable windows remoting service to log in as a different user to run tests
+powershell -Command 'Start-Service -Name WinRM' > /dev/null
+powershell -Command 'Start-Service -Name seclogon' > /dev/null
+
+# change settings to allow a blank password for TEST_USER
+reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Lsa //t REG_DWORD //v LimitBlankPasswordUse //d 0 //f 2>&1 > /dev/null
+net accounts /minpwlen:0 > /dev/null
+
+# create the test user with a space in the username
+echo "Creating user: \"test user\"..."
+TEST_USER="test user"
+echo > "$HOME/test_user_creds"
+net user "$TEST_USER" //ADD "//homedir:\\\\%computername%\\users\\$TEST_USER" > /dev/null < "$HOME/test_user_creds"
+
+# add to administrators group
+net localgroup administrators "$TEST_USER" //add > /dev/null
+
+# run a command as new user to create home directory
+runas //user:"$TEST_USER" "cmd /C whoami" > /dev/null < "$HOME/test_user_creds"
+
+# copy hnn source to test user's home directory
+TEST_USER_DIR="/c/Users/$TEST_USER"
+if [ -d "$TEST_USER_DIR" ]; then
+  cp -r "$(pwd)" "$TEST_USER_DIR/"
+else
+  echo "No user home directory created at $TEST_USER_DIR"
+  exit 2
+fi
