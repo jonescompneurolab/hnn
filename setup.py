a72e7a92df795b1bd2eed4a2f09312b55be4ffbe
Blake Caldwell <blake_caldwell@brown.edu>
2020-10-04 23:43:31 -0400

    MAINT: update setup.py to include .params

--- setup.py
+++ setup.py
@@ -1,18 +1,39 @@
-import setuptools
+#! /usr/bin/env python
 
-setuptools.setup(
-    name="hnn", # Replace with your own username
-    version="1.4.0",
-    author="Blake Caldwell",
-    author_email="blake_caldwell@brown.edu",
-    description="Human Neocortical Neurosolver",
-    long_description='',
-    url="https://github.com/jonescompneurolab/hnn",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: Brown CS License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+from setuptools import setup, find_packages
+
+descr = """Human Neocortical Neurosolver"""
+
+DISTNAME = 'hnn'
+DESCRIPTION = descr
+MAINTAINER = 'Blake Caldwell'
+MAINTAINER_EMAIL = 'blake_caldwell@brown.edu'
+URL = ''
+LICENSE = 'Brown CS License'
+DOWNLOAD_URL = 'http://github.com/jonescompneurolab/hnn'
+VERSION = '1.4.0'
+
+if __name__ == "__main__":
+    setup(name=DISTNAME,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          long_description=open('README.md').read(),
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved',
+              'Programming Language :: Python',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS',
+          ],
+          platforms='any',
+          packages=find_packages(),
+          package_data={'hnn':
+                        ['../param/*.param']})
