e1c0683086e74a5105c6b0dc1fa4af5ac124f563
Blake Caldwell <blake_caldwell@brown.edu>
2020-10-04 23:10:18 -0400

    MAINT: removing linux uninstall scripts

--- installer/ubuntu/uninstaller.sh
+++
@@ -1,5 +0,0 @@
-# clean up the bashrc (sed looks like it uses regex, but I don't
-# think that it does. You do have to escape the '/' character, though)
-sed -i '/# these lines define global session variables for HNN/d' ~/.bashrc
-sed -i '/export CPU=$(uname -m)/d' ~/.bashrc
-sed -i "/export PATH=\$PATH:$startdir\/nrn\/build\/\$CPU\/bin/d" ~/.bashrc
